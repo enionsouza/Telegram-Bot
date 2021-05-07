e90d0e0534bc6c33d02a668897646c2e33492cc2
Enio N. de Souza <enionsouza@gmail.com>
2021-05-07 18:04:53 -0400

    Add humor functionality

--- bin/main.rb
+++ bin/main.rb
@@ -4,8 +4,10 @@ require 'json'
 require 'telegram/bot'
 require_relative '../lib/robot'
 require_relative '../lib/inspiration'
+require_relative '../lib/humor'
 
 quotes = Inspiration.new.inspiration
+jokes = Humor.new.humor
 
 Telegram::Bot::Client.run(CapstoneRobot.token) do |bot|
   bot.listen do |message|
@@ -21,13 +23,26 @@ Telegram::Bot::Client.run(CapstoneRobot.token) do |bot|
         text: 'Please, choose one of the following categories:',
         reply_markup: answers
       )
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    when '/just_for_fun'
+      bot.api.send_message(chat_id: message.chat.id, text: jokes[jokes.keys.sample].sample)
+    when '/amuse_me'
+      answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: jokes.keys, one_time_keyboard: true)
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: 'Okay, fun time! Choose one of the following categories:',
+        reply_markup: answers
+      )
     else
       if quotes.keys.include? message.text
         bot.api.send_message(
           chat_id: message.chat.id,
           text: quotes[message.text].sample
+        )
+      end
+      if jokes.keys.include? message.text
+        bot.api.send_message(
+          chat_id: message.chat.id,
+          text: jokes[message.text].sample
         )
       end
     end
---
+++ lib/humor.rb
@@ -0,0 +1,37 @@
+require 'json'
+require 'nokogiri'
+require 'open-uri'
+
+class Humor
+  attr_reader :humor
+
+  def initialize
+    file_name = '../src/humor.json'
+    scrape_for_jokes unless File.exist?(file_name)
+    @humor = JSON.parse(File.read(file_name))
+  end
+
+  def scrape_for_jokes
+    doc = Nokogiri::HTML(URI.open('https://www.countryliving.com/life/a27452412/best-dad-jokes/'))
+    dad_jokes = []
+    doc.css('.article-body-content li').each { |joke| dad_jokes.push(joke.content) }
+
+    doc = Nokogiri::HTML(URI.open('https://bestlifeonline.com/funny-short-jokes/'))
+    short_jokes = []
+    doc.css('.main-content .noskimwords li').each { |joke| short_jokes.push(joke.content) }
+
+    doc = Nokogiri::HTML(URI.open('https://www.readersdigest.ca/culture/10-short-jokes-anyone-can-remember/'))
+    img_jokes = []
+    doc.css('.listicle-card-image-wrapper img').each do |img|
+      img_jokes.push(img.attr('src')) unless img.attr('src')[0] == 'd'
+    end
+
+    get_jokes = {
+      'Dad jokes' => dad_jokes ,
+      'Short jokes' => short_jokes,
+      'Image jokes' => img_jokes
+    }
+    Dir.mkdir('../src/') unless Dir.children('..').include? 'src'
+    File.write('../src/humor.json', get_jokes.to_json)
+  end
+end
