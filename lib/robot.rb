52f0aa2c40ec371e6c7184896e36225ce0c417c7
Enio N. de Souza <enionsouza@gmail.com>
2021-05-10 15:10:17 -0400

    Remove robot.rb due to sensitive information privacy

--- lib/robot.rb
+++
@@ -1,9 +0,0 @@
-module CapstoneRobot
-  @name = 'Capstone Project - Ruby Module'
-  @username = '@enionsouza_bot'
-  @token = '1753111389:AAFBAiWdx4SeY5YRO19UhS8cqzCcTW8_x60'
-
-  def self.token
-    @token
-  end
-end
