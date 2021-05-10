262ed6be539465c2977a5145588cf64d853a8f07
Enio N. de Souza <enionsouza@gmail.com>
2021-05-10 15:12:03 -0400

    Add functionalities to the robot

--- bin/main.rb
+++ bin/main.rb
@@ -3,25 +3,53 @@
 
 require 'json'
 require 'telegram/bot'
-require_relative '../lib/robot'
 require_relative '../lib/inspiration'
 require_relative '../lib/humor'
 
 quotes = Inspiration.new.inspiration
 jokes = Humor.new.humor
+begin
+  TOKEN = File.read('../config/robot_token.txt')
+rescue StandardError
+  puts 'Please, refer to README.md to properly configure your robot...'
+end
 
-Telegram::Bot::Client.run(CapstoneRobot.token) do |bot|
+Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        parse_mode: 'HTML',
+        text: "Hello, <strong>#{message.from.first_name}</strong>! I'm your inspiring robot!
+Please enter <strong>/help</strong> for more information!")
+    when '/describe'
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        parse_mode: 'HTML',
+        text: "<strong>I will keep you up with inspiring quotes and funny jokes!</strong>
+<em>Inspirational quotes</em> are one way to help stay positive, productive, and happy as you move along your journey.
+Even saying one daily motivational quote in the mirror each more can make a huge impact on your day-to-day life.
+Additionally, keep in mind that old saying: <em>\"Laughter is the Best Medicine\"</em>.
+Laughter strengthens your immune system, diminishes pain, and protects you from the damaging effects of stress.
+<strong>So, let's keep our mood up!</strong>")
     when '/help', '/menu'
       bot.api.send_message(
         chat_id: message.chat.id,
+        parse_mode: 'HTML',
         text: "/inspire_me - Displays a menu for Inspiring Quotes Categories
 /amuse_me - Displays a menu for Jokes Categories
-/fortune_cookie - Presents a Inspiring Quote randomly selected
-/just_for_fun - Presents a Joke randomly selected"
+/fortune_cookie - Picks a Inspiring Quote randomly selected
+/just_for_fun - Picks a Joke randomly selected
+/describe - Presents a description of this Robot or <em>'What can this robot do?'</em>
+/about - Presents information about this Robot"
+      )
+    when '/about'
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        parse_mode: 'HTML',
+        text: "<b>Microverse Capstone Project - Ruby Module</b>
+<em>I will keep you up with inspiring quotes and funny jokes!</em>"
       )
     when '/fortune_cookie'
       bot.api.send_message(chat_id: message.chat.id, text: quotes[quotes.keys.sample].sample)
