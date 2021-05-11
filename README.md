fc2089c41e7174751700a4446067283f456b8691
Enio N. de Souza <enionsouza@gmail.com>
2021-05-11 15:27:55 -0400

    Perform changes required by the code reviewers

--- README.md
+++ README.md
@@ -10,8 +10,8 @@
 
 **I will keep you up with inspiring quotes and funny jokes!**
 
-> Inspirational quotes are one way to help stay positive, productive, and happy as you move along your journey. Even saying one daily motivational quote in the mirror each more can make a huge impact on your day-to-day life.
-> Additionally, keep in mind that old saying: "Laughter is the Best Medicine". Laughter strengthens your immune system, boosts mood, diminishes pain, and protects you from the damaging effects of stress. So, let's keep our mood up!
+> Inspirational quotes are one way to help you to stay positive, productive, and happy as you move along your journey. Even saying one motivational quote to yourself in front of the mirror each day can make an enormous impact on your day-to-day life.
+> Additionally, keep in mind that old saying: "Laughter is the Best Medicine". Laughter strengthens your immune system, boosts mood, diminishes pain, and protects you from the damaging effects of stress. So, let's keep our attitude up!
 
 ## Built With
 
@@ -24,7 +24,7 @@
 
 ## Live Demo
 
-To avoid the disclosure of the API Token used in this project, no live demo was provided. _Please, follow the steps listed below to run this app on your machine_.
+To avoid the disclosure of the API Token used in this project, no live demo was provided. _Please follow the steps listed below to run this app on your machine_.
 
 ## Getting Started
 
@@ -33,7 +33,7 @@ To get a local copy up and running, follow these simple example steps:
 ### Prerequisites
 
 - You should have Ruby (version 3.0.1 or higher) installed on your computer.
-- You should also have an account on Telegram and you should also create a new robot on the platform, as showed bellow:
+- Additionally, you should have an account on Telegram and you'll have to create a new robot on the platform, as shown below:
 
 ### Create your Telegram Robot
 
@@ -42,8 +42,8 @@ To run this project, you'll need to create your rob
 - On Telegram, search for [@BotFather](https://t.me/BotFather), this is an app provided by Telegram for creating bots
   ![screenshot](./img/Screenshot-BotFather.png)
 - Once you've started BotFather, enter `/newbot`
-- Then, enter your bot name and a username. Notice that the bot's username should be finished with `bot`
-- Now you should recieve an API Token. Save this information for later use.
+- Then, enter your bot name and a username. Notice that the bot's username should end with `bot`
+- Now, you should receive an API Token from BotFather. Save this information for later use.
 - Finally, search for your newly created robot on Telegram and start it!
 
 ### Install
@@ -51,7 +51,7 @@ To run this project, you'll need to create your rob
 - On your bash, go to the directory of your preference and run `git clone git@github.com:enionsouza/Telegram-Bot.git`
 - Next run `cd Telegram-Bot/config`
 - Open the file named `robot_token-example.txt` with the editor of your preference.
-- Now, replace the provided example of API Token with the token that you've receirved from BotFather, in the previous step. Save this edited file in this same directory with the name `robot_token.txt`
+- Now, replace the generic example of API Token with the token you've received from BotFather, in the previous step. Save this edited file in this same directory with the name `robot_token.txt`
 - Next, on your bash, run `cd ..`
 - Finally, to run the required gems used in this project run `bundle install`
 
@@ -64,15 +64,16 @@ To run this project, you'll need to create your rob
 - Now that you're in `Telegram-Bot` directory, run `cd bin`
 - To turn your Telegram Robot on, either run `./main.rb` or `ruby main.rb`
 - Now go to your Telegram Robot and enter `/help`. The other available commands should appear on your screen, as the robot's response
+- To stop your Telegram Robot, hit `command + Z` or `CTRL + Z` on your keyboard
 
 ### About the quotes and jokes sources
 
-- For this project, the quotes and jokes were web scraped from the following pages:
+- For this project, the quotes and jokes were web-scraped from the following pages:
   - [Get Inspired: 200+ Motivational Quotes To Encourage Your Success](https://www.shopify.com/blog/motivational-quotes)
   - [145 Best Dad Jokes That Will Have the Whole Family Laughing](https://www.countryliving.com/life/a27452412/best-dad-jokes/)
   - [50 Funny Short Jokes That Guarantee a Laugh](https://bestlifeonline.com/funny-short-jokes/)
   - [75 Short Jokes Anyone Can Remember](https://www.readersdigest.ca/culture/10-short-jokes-anyone-can-remember/)
-- The web scraping was automatically performed and, _for testing purposes_, you can also delete the directory named `src` along with all its content files. When `./main.rb` is run in the `bin` directory, new source files will be regenerated from the mentioned sources. You can also re-run the RSpec tests, for sanity check of the newly generated source files.
+- The web scraping was automatically performed and, _for testing purposes_, you can also delete the directory named `src` along with all its content files. When `./main.rb` is run in the `bin` directory, identical new `json` files will be generated from the mentioned sources. You can also re-run the RSpec tests, just for the sake of sanity check of the newly generated `json` files.
 
 ## Author
 
@@ -96,7 +97,7 @@ Give a ⭐️ if you like this project!
 
 - My beloved family, for all the support provided!
 - [@fernandorpm](https://github.com/fernandorpm), for the valuable tips! Thanks, my mentor!
-- Microverse Support Team
+- Microverse Team
 
 ## 📝 License
 
--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,65 @@
 #!/usr/bin/env ruby
-# rubocop:disable Metrics/BlockLength
 
 require 'json'
 require 'telegram/bot'
 require_relative '../lib/inspiration'
 require_relative '../lib/humor'
+
+def start(bot, id, first_name)
+  bot.api.send_message(
+    chat_id: id,
+    parse_mode: 'HTML',
+    text: "Hello, <strong>#{first_name}</strong>! I'm your inspiring robot!
+Please enter <strong>/help</strong> for more information!"
+  )
+end
+
+def describe(bot, id)
+  bot.api.send_message(
+    chat_id: id,
+    parse_mode: 'HTML',
+    text: "<strong>I will keep you up with inspiring quotes and funny jokes!</strong>
+<em>Inspirational quotes</em> are one way to help you to stay positive and productive as you move along your journey.
+Even saying one motivational quote to yourself in front of the mirror every day can make a huge impact on your life.
+Additionally, keep in mind that old saying: \"Laughter is the Best Medicine\".
+Laughter strengthens your immune system, diminishes pain, and protects you from the damaging effects of stress.
+<strong>So, let's keep our attitude up!</strong>"
+  )
+end
+
+def help(bot, id)
+  bot.api.send_message(
+    chat_id: id,
+    parse_mode: 'HTML',
+    text: "/inspire_me - Displays a menu for Inspiring Quotes Categories
+/amuse_me - Displays a menu for Jokes Categories
+/fortune_cookie - Picks a Inspiring Quote randomly selected
+/just_for_fun - Picks a Joke randomly selected
+/describe - Presents a description of this Robot or <em>'What can this robot do?'</em>
+/about - Presents information about this Robot"
+  )
+end
+
+def about(bot, id)
+  bot.api.send_message(
+    chat_id: id,
+    parse_mode: 'HTML',
+    text: "<b>Microverse Capstone Project - Ruby Module</b>
+<em>I will keep you up with inspiring quotes and funny jokes!</em>"
+  )
+end
+
+def send_message(bot, id, message)
+  bot.api.send_message(chat_id: id, text: message)
+end
+
+def menu(bot, id, options)
+  bot.api.send_message(
+    chat_id: id,
+    text: 'Please, choose one of the following categories:',
+    reply_markup: options
+  )
+end
 
 quotes = Inspiration.new.inspiration
 jokes = Humor.new.humor
@@ -18,73 +73,26 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
     case message.text
     when '/start'
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        parse_mode: 'HTML',
-        text: "Hello, <strong>#{message.from.first_name}</strong>! I'm your inspiring robot!
-Please enter <strong>/help</strong> for more information!"
-      )
+      start(bot, message.chat.id, message.from.first_name)
     when '/describe'
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        parse_mode: 'HTML',
-        text: "<strong>I will keep you up with inspiring quotes and funny jokes!</strong>
-<em>Inspirational quotes</em> are one way to help stay positive, productive, and happy as you move along your journey.
-Even saying one daily motivational quote in the mirror each more can make a huge impact on your day-to-day life.
-Additionally, keep in mind that old saying: <em>\"Laughter is the Best Medicine\"</em>.
-Laughter strengthens your immune system, diminishes pain, and protects you from the damaging effects of stress.
-<strong>So, let's keep our mood up!</strong>"
-      )
-    when '/help', '/menu'
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        parse_mode: 'HTML',
-        text: "/inspire_me - Displays a menu for Inspiring Quotes Categories
-/amuse_me - Displays a menu for Jokes Categories
-/fortune_cookie - Picks a Inspiring Quote randomly selected
-/just_for_fun - Picks a Joke randomly selected
-/describe - Presents a description of this Robot or <em>'What can this robot do?'</em>
-/about - Presents information about this Robot"
-      )
+      describe(bot, message.chat.id)
+    when '/help'
+      help(bot, message.chat.id)
     when '/about'
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        parse_mode: 'HTML',
-        text: "<b>Microverse Capstone Project - Ruby Module</b>
-<em>I will keep you up with inspiring quotes and funny jokes!</em>"
-      )
+      about(bot, message.chat.id)
     when '/fortune_cookie'
-      bot.api.send_message(chat_id: message.chat.id, text: quotes[quotes.keys.sample].sample)
+      send_message(bot, message.chat.id, quotes[quotes.keys.sample].sample)
     when '/inspire_me'
-      answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: quotes.keys, one_time_keyboard: true)
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        text: 'Please, choose one of the following categories:',
-        reply_markup: answers
-      )
+      options = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: quotes.keys, one_time_keyboard: true)
+      menu(bot, message.chat.id, options)
     when '/just_for_fun'
-      bot.api.send_message(chat_id: message.chat.id, text: jokes[jokes.keys.sample].sample)
+      send_message(bot, message.chat.id, jokes[jokes.keys.sample].sample)
     when '/amuse_me'
-      answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: jokes.keys, one_time_keyboard: true)
-      bot.api.send_message(
-        chat_id: message.chat.id,
-        text: 'Okay, fun time! Choose one of the following categories:',
-        reply_markup: answers
-      )
+      options = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: jokes.keys, one_time_keyboard: true)
+      menu(bot, message.chat.id, options)
     else
-      if quotes.keys.include? message.text
-        bot.api.send_message(
-          chat_id: message.chat.id,
-          text: quotes[message.text].sample
-        )
-      end
-      if jokes.keys.include? message.text
-        bot.api.send_message(
-          chat_id: message.chat.id,
-          text: jokes[message.text].sample
-        )
-      end
+      send_message(bot, message.chat.id, quotes[message.text].sample) if quotes.keys.include? message.text
+      send_message(bot, message.chat.id, jokes[message.text].sample) if jokes.keys.include? message.text
     end
   end
 end
-# rubocop:enable Metrics/BlockLength
